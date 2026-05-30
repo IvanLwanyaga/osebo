c436d03e08b51d5538f49cf223acf31c3faf642b
Ivan Lwanyaga <lwanyaga10ivan@gmail.com>
2026-05-31 00:02:44 +0300

    feat: add Shop data class for shop representation

---
+++ app/src/main/java/com/osebo/ai/models/shop.kt
@@ -0,0 +1,9 @@
+package com.osebo.ai.models
+
+data class Shop(
+    val id: String = "",
+    val name: String = "",
+    val address: String = "",
+    val phoneNumber: String = "",
+    val ownerId: String = ""
+)
