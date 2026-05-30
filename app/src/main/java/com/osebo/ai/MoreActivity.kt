c9cc5f75471bcbd4756ef73fcac69e3af3b6a92e
Ivan Lwanyaga <lwanyaga10ivan@gmail.com>
2026-05-30 22:26:19 +0300

    feat: create MoreActivity for additional app features

---
+++ app/src/main/java/com/osebo/ai/MoreActivity.kt
@@ -0,0 +1,11 @@
+package com.osebo.ai
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more)
+    }
+}
